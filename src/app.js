3a3e13f6665cd60db5683a18e8a3f3cdc4d244e7
Lee2103 <k.lee@windowslive.com>
2021-12-27 13:44:38 +0000

    Updated weather function and corresponding CSS and HTML

--- src/app.js
+++ src/app.js
@@ -1,18 +1,17 @@
-let apiKey = "d71484783f68fd1d93c40c841c6cd692";
-
-function formatTime(time) {
-  let hours = time.getHours();
+function formatTime(timestamp) {
+  let date= new Date(timestamp);
+  let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  let minutes = time.getMinutes();
+  let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
   return `${hours}:${minutes}`;
 }
-
-function formatDate(date) {
+function formatDate(timestamp) {
+  let date= new Date(timestamp);
   let dayIndex = date.getDay();
   let days = [
     "Sunday",
@@ -43,8 +42,77 @@ function formatDate(date) {
   let month = months[monthIndex];
   return `${day} ${dateNumber} ${month}`;
 }
+
+function displayWeather(response) {
+  let temperatureElement= document.querySelector("#temperature");
+  let windSpeedElement= document.querySelector ("#wind-speed");
+  let humidityElement= document.querySelector ("#humidity");
+  let weatherConditionElement= document.querySelector ("#weather-condition");
+  let iconElement= document.querySelector ("#icon");
+  let cityElement= document.querySelector ("#city");
+  let dateElement= document.querySelector ("#date");
+  let timeElement= document.querySelector ("#time")
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML= Math.round (celsiusTemperature);
+  windSpeedElement.innerHTML= Math.round(response.data.wind.speed);
+  humidityElement.innerHTML=response.data.main.humidity;
+  weatherConditionElement.innerHTML=response.data.weather[0].description;
+  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  cityElement.innerHTML=response.data.name;
+  dateElement.innerHTML= formatDate(response.data.dt*1000);
+  timeElement.innerHTML= formattime(response.data.dt*1000);
+}
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+function search (city) {
+  let apiKey= "d71484783f68fd1d93c40c841c6cd692";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeather);
+}
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+function handleSubmit (event) {
+  event.preventDefault();
+  let cityInputElement= document.querySelector ("#city-input");
+  search(cityInputElement.value);
+}
+
 let dateElement = document.querySelector("#date");
 let timeElement = document.querySelector("#time");
 let currentTime = new Date();
 dateElement.innerHTML = formatDate(currentTime);
 timeElement.innerHTML = formatTime(currentTime);
+
+let form= document.querySelector ("#search-form");
+form.addEventListener ("submit", handleSubmit);
+
+search ("Tokyo");
+
+
+
+
